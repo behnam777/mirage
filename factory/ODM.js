04310b9ecb17f2b8c88a0619e967370292500376
behnam1070 <behnam1070@gmail.com>
2022-11-11 21:33:58 +0000

    MirageV1

--- factory/ODM.js
+++ factory/ODM.js
@@ -6,7 +6,7 @@ ODM.modelMaker = async (entityName , entityModel)=>{
         if(entityName && entityModel && Object.keys(entityModel).length){  
             const schema = new  mongoose.Schema({ ...entityModel }); 
             console.log( schema);
-            global.Models[entityName] = mongoose.model("user" , schema );
+            global.Models[entityName] = mongoose.model("user"  , schema );
         }
     } catch (error) { return false}
 }
